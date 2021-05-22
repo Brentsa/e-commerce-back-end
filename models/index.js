3717ada545f7fe438a025adc2b7b36d76a389672
Solidshots <seanbrent5@gmail.com>
2021-05-22 16:08:13 -0400

    get products

--- models/index.js
+++ models/index.js
@@ -5,31 +5,22 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Category, {foreignKey: 'category_id'});
+Product.belongsTo(Category);
 
 // Categories have many Products
-Category.hasMany(Product, {foreignKey: 'category_id'});
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, {
-  through: ProductTag,
-  as: 'product_tags',
-  foreignKey: 'product_id'
-});
+Product.belongsToMany(Tag, {through: ProductTag});
 
-ProductTag.belongsTo(Product, {foreignKey: 'product_id'});
-Product.hasMany(ProductTag, {foreignKey: 'product_id'});
-
+ProductTag.belongsTo(Product);
+Product.hasMany(ProductTag);
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, {
-  through: ProductTag,
-  as: 'product_tags',
-  foreignKey: 'tag_id'
-});
+Tag.belongsToMany(Product, {through: ProductTag});
 
-ProductTag.belongsTo(Tag, {foreignKey: 'tag_id'});
-Tag.hasMany(ProductTag, {foreignKey: 'tag_id'});
+ProductTag.belongsTo(Tag);
+Tag.hasMany(ProductTag);
 
 module.exports = {
   Product,
@@ -37,3 +28,26 @@ module.exports = {
   Tag,
   ProductTag,
 };
+
+// // Products belongToMany Tags (through ProductTag)
+// Product.belongsToMany(Tag, {
+//   through: ProductTag,
+//   as: 'product_tags',
+//   foreignKey: 'product_id'
+// });
+
+// ProductTag.belongsTo(Product, {foreignKey: 'product_id'});
+// Product.hasMany(ProductTag, {foreignKey: 'product_id'});
+
+
+// // Tags belongToMany Products (through ProductTag)
+// Tag.belongsToMany(Product, {
+//   through: ProductTag,
+//   as: 'product_tags',
+//   foreignKey: 'tag_id'
+// });
+
+// ProductTag.belongsTo(Tag, {foreignKey: 'tag_id'});
+// Tag.hasMany(ProductTag, {foreignKey: 'tag_id'});
+
+// Products belongToMany Tags (through ProductTag)
